b191fd4adad2f4faefbef49fc2c5f69332041b65
Balamurugan <balamurugan@divum.in>
2022-06-14 14:42:29 +0530

    Chore: issues fixed

--- src/alarm.js
+++ src/alarm.js
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import {NativeModules} from 'react-native';
+import 'react-native-get-random-values';
 import {v4 as uuidv4} from 'uuid';
 
 const AlarmService = NativeModules.AlarmModule;
